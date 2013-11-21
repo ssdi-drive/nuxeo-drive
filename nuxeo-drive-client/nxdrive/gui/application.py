6eb10b276dbb69ef269fccb3151ffc748c614fd3
Jiakuan Wang <delight.wjk@gmail.com>
2013-11-21 15:49:10 +0800

    NXP-13443: Add logic to show message to user when no local file found for live editing

--- nuxeo-drive-client/nxdrive/gui/application.py
+++ nuxeo-drive-client/nxdrive/gui/application.py
@@ -38,6 +38,9 @@ class Communicator(QObject):
     stop = QtCore.pyqtSignal()
     invalid_credentials = QtCore.pyqtSignal(str)
 
+    # https://jira.nuxeo.com/browse/SUPNXP-9065
+    # For message notification (msg_title, msg_body)
+    message = QtCore.pyqtSignal(str, str)
 
 class BindingInfo(object):
     """Summarize the state of each server connection"""
@@ -96,6 +99,7 @@ class Application(QApplication):
         self.communicator.stop.connect(self.handle_stop)
         self.communicator.invalid_credentials.connect(
             self.handle_invalid_credentials)
+        self.communicator.message.connect(self.show_message)
 
         # Timer to spin the transferring icon
         self.icon_spin_timer = QtCore.QTimer()
@@ -113,6 +117,11 @@ class Application(QApplication):
         self.global_menu_actions = {}
         self.update_menu()
         self._tray_icon.setContextMenu(self.tray_icon_menu)
+
+        # Register frontend into the controller so that user message can be
+        # notified inside the controller. For more information, please see
+        # https://jira.nuxeo.com/browse/SUPNXP-9065
+        self.controller.register_frontend(self)
 
         # Start long running synchronization thread
         self.start_synchronization_thread()
@@ -286,6 +295,12 @@ class Application(QApplication):
                 info.online = True
                 self.update_running_icon()
                 self.communicator.menu.emit()
+
+    def notify_user_message(self, msg_title, msg_body):
+        if msg_title is not None and msg_body is not None:
+            self.communicator.message.emit(msg_title, msg_body)
+        else:
+            log.error("Both message title and message body cannot be empty")
 
     def _setup_systray(self):
         self._tray_icon = QtGui.QSystemTrayIcon()
@@ -489,6 +504,17 @@ class Application(QApplication):
         version = self.controller.get_version()
         return prompt_settings(self.controller, sb_settings, proxy_settings,
                                version, app=self)
+    @QtCore.pyqtSlot(str, str)
+    def show_message(self, msg_title, msg_body):
+        """Show a message in systray area.
+
+        Currently, the main use is for displaying a user-friendly message when
+        opening a file with ndrive that is not yet downloaded. For more info
+        please see this ticket: https://jira.nuxeo.com/browse/SUPNXP-9065
+
+        """
+        self._tray_icon.showMessage(msg_title, msg_body)
+        return True
 
     def start_synchronization_thread(self):
         if len(self.controller.list_server_bindings()) == 0:
